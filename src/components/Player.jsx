08ebf671b9526bcdcc3e46b8a88937936fbf8a0c
leomcg <leomcg@gmail.com>
2023-12-11 15:06:22 -0300

    adding challenge cards

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("unknown entity");
+  const playerNameInput = useRef();
+
+  function handleClick() {
+    setPlayerName(playerNameInput.current.value);
+    playerNameInput.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
